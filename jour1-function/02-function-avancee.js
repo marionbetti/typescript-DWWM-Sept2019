c52031bf3f2672583c8ede3165e50489c8d5d32c
babacar <babacr@mail.com>
2019-09-16 15:03:25 +0200

    fonction avec argument facultatif

--- jour1-function/02-function-avancee.js
+++ jour1-function/02-function-avancee.js
@@ -1,8 +1,23 @@
 "use strict";
 exports.__esModule = true;
-function Greeting(prenom, titre) {
+function greeting(prenom, titre) {
     var phrase = "Bonjour " + titre + " " + prenom;
     return phrase;
 }
-var r = Greeting("Alain", "M.");
+var r = greeting("Alain", "M.");
 console.log(r.toLocaleUpperCase());
+// fonction avec argument facultatif et retourne du texte 
+function planning(complet) {
+    if (complet)
+        return [
+            { id: 1, jour: "Lundi", sujet: "NodeJS" },
+            { id: 2, jour: "Mardi", sujet: "Typescript" }
+        ];
+    else
+        return [
+            { sujet: "NodeJS" },
+            { sujet: "Typescript" }
+        ];
+}
+console.log(planning()); // version courte
+console.log(planning(true)); // version longue
--- jour1-function/02-function-avancee.ts
+++ jour1-function/02-function-avancee.ts
@@ -1,11 +1,31 @@
 // tsc 02-function-avancee.ts -w
 export{}
 
-function Greeting (prenom:string, titre:string) :string
+function greeting (prenom: string, titre: string) : string
 {
     const phrase = `Bonjour ${titre} ${prenom}`;
     return phrase;
 }
 
-const r :string  = Greeting("Alain", "M.");
+const r :string  = greeting("Alain", "M.");
 console.log(r.toLocaleUpperCase());
+
+// fonction avec argument facultatif et retourne du texte 
+
+function planning (complet ?: boolean) : Array<Object>
+{
+ if(complet )
+    return [
+        {id:1, jour:"Lundi",sujet:"NodeJS"},
+        {id:2, jour:"Mardi",sujet:"Typescript"}
+    ]
+ else
+    return [
+        {sujet:"NodeJS"},
+        {sujet:"Typescript"}
+    ]
+}
+
+
+console.log(planning()) // version courte
+console.log(planning(true)) // version longue
