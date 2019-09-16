2814f0878658370169fa368549f9f4a520d8222c
babacar <babacr@mail.com>
2019-09-16 12:08:35 +0200

    jamais utiliser var dans les fichiers ts

---
+++ jour1-variables/01-declaration.ts
@@ -0,0 +1,16 @@
+// tsc 01-declarations.ts -w
+
+// svp (pour vous !!) ne plus utiliser var lorsque vous créez 
+//des variables
+
+const a = "Bonjour";
+a = 2 ;
+
+// variable qui va changer de valeur => let (plus var)
+
+let b = 3; // + il va affecter un type 
+//b va récupérer des méthodes du type number
+b = "hello";
+b.toFixed(2);
+
+// JAMAIS var 
