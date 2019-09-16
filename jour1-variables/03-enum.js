ffa6ebb524dffbde9f1710f19eed7b7382bcf874
babacar <babacr@mail.com>
2019-09-16 12:52:12 +0200

    découverte enum ts

---
+++ jour1-variables/03-enum.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+// ajouté par Typescript
+// attention pas de = 
+var couleur;
+(function (couleur) {
+    couleur["bg"] = "#43ab92";
+    couleur["police"] = "#f75f00";
+    couleur["logo"] = "#c93838";
+})(couleur || (couleur = {}));
+// google => colorhunt = https://colorhunt.co/
+// enum = constante complexe
+couleur.logo;
+couleur.bg;
---
+++ jour1-variables/03-enum.ts
@@ -0,0 +1,16 @@
+//tsc 03-enum.ts -w
+export {}
+// ajouté par Typescript
+// attention pas de = 
+enum couleur {
+    bg = "#43ab92",
+    police = "#f75f00",
+    logo = "#c93838"
+}
+
+// google => colorhunt = https://colorhunt.co/
+// enum = constante complexe
+
+couleur.logo
+
+couleur.bg
