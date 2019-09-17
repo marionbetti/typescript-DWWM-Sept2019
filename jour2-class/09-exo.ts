d7b15e3cca07160623b02f57c422d730d8483360
malik <malik.h@webdevpro.net>
2019-09-17 14:51:46 +0200

    exo class

---
+++ jour2-class/09-exo.ts
@@ -0,0 +1,22 @@
+/* créer une class Article
+
+4 proprietes 
+titre
+contenu 
+auteur 
+etat Publié on non publié 
+
+méthode pour générer un article dans la homepage
+
+<h2>titre</h2>
+<p>  30 lettres du contenu </p>
+<footer><a href="#">lire la suite ... </a></footer>
+
+si article n'est pas publié => dans la homepage ""
+
+méthode pour générer un article dans une page 
+<h2>titre</h2>
+<div>  contenu </div>
+<footer><strong>Auteur</strong></footer>
+
+si article n'est pas publié => <p>Erreur 404 page non disponible</p>  */
