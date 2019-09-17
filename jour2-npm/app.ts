a88aac8ef97960b81109b8d782f2da640f43abf3
malik <malik.h@webdevpro.net>
2019-09-17 17:03:10 +0200

    utiliser,lodash dans unprojet ts

---
+++ jour2-npm/app.ts
@@ -0,0 +1,9 @@
+import * as _ from "lodash" ; 
+
+// pour pouvoir utiliser une librairie téléchargée
+// télécharger la libraire 
+// npm i lodash
+// télécharger les fichiers de définitions
+// npm i @types/lodash -D
+
+_.random(0, 1000);
