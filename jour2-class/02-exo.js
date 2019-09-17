b64a143a1c0dd62ac4810724115a34f6b6d3227a
malik <malik.h@webdevpro.net>
2019-09-17 11:26:56 +0200

    exo corrige part 1

--- jour2-class/02-exo.js
+++ jour2-class/02-exo.js
@@ -1,11 +1,9 @@
 // Créer une class 
 //nom Fenetre
-
 // trois propriétés
 // titre : chaine de caractère
 // body :  chaine de caractère
 // id : chaine de caractère
-
 // un méthode :
 // nom afficher
 // traitement suivants :
@@ -14,39 +12,21 @@
 //    <header>la variable titre de la class Fenetre</header>
 //    <div>la variable body de la class Fenetre</div>
 //</section>
-
 // créer la fenêtre titre Panneau de configuration / body liste <ul><li> / id config
 // créer la fenêtre titre option / body liste <ul><li> / id option
-
 // https://github.com/webdevproformation/typescript-DWWM-Sept2019
-
-class Fenetre
-{
-    titre : string;
-    body : string;
-    id : string;
-
-    afficher() :void
-    {
-        const html = `
-            <section id="${this.id}">
-                <header>${this.titre}</header>
-                <div>${this.body}</div>
-            </section>
-        `;
-        console.log(html) ;
+var Fenetre = /** @class */ (function () {
+    function Fenetre() {
     }
-}
-
-const f1 = new Fenetre();
+    Fenetre.prototype.afficher = function () {
+        var html = "\n            <section id=\"" + this.id + "\">\n                <header>" + this.titre + "</header>\n                <div>" + this.body + "</div>\n            </section>\n        ";
+        console.log(html);
+    };
+    return Fenetre;
+}());
+var f1 = new Fenetre();
 f1.titre = "Panneau de configuration";
-f1.body = `
-<ul>
-    <li><a href="admin.html">Admin</a></li>
-    <li><a href="login.html">login</a></li>
-    <li><a href="logout.html">déconnexion</a></li>
-</ul>
-`;
-f1.id= "config" ;
+f1.body = "\n<ul>\n    <li><a href=\"admin.html\">Admin</a></li>\n    <li><a href=\"login.html\">login</a></li>\n    <li><a href=\"logout.html\">d\u00E9connexion</a></li>\n</ul>\n";
+f1.id = "config";
 f1.afficher();
 //tsc 02-exo.ts -w
