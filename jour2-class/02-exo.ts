f97e04105744701d69b7051169e7f9eda97eb71d
malik <malik.h@webdevpro.net>
2019-09-17 10:59:48 +0200

    exo class

---
+++ jour2-class/02-exo.ts
@@ -0,0 +1,19 @@
+// Créer une class 
+//nom Fenetre
+
+// trois propriétés
+// titre : chaine de caractère
+// body :  chaine de caractère
+// id : chaine de caractère
+
+// un méthode :
+// nom afficher
+// traitement suivants :
+// afficher dans la console la chaine suivante :
+//<section id="la variable id de la class Fenetre">
+//    <header>la variable titre de la class Fenetre</header>
+//    <div>la variable de la class Fenetre</div>
+//</section>
+
+// créer la fenêtre titre Panneau de configuration / body liste <ul><li> / id config
+// créer la fenêtre titre option / body liste <ul><li> / id option
