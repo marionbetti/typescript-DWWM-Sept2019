2d915cc2c5a464d48b405edbdb30f639f860b1a9
malik <malik.h@webdevpro.net>
2019-09-17 11:53:51 +0200

    rappel if et concatenation

---
+++ jour2-class/03-rappels.ts
@@ -0,0 +1,33 @@
+
+let expression :boolean = 10 < 20;
+//let expression :boolean = true;
+
+
+if( true )
+{
+   alert("ici")
+}
+
+if (expression) alert("ici")
+
+if(expression)
+{
+    alert("la");
+}
+else
+{
+    alert("lala");
+}
+
+(expression) ? alert("la") :  alert("lala") ; // écriture du if ternaire
+export {}
+let a = 1
+let b = 2
+let c = a + b ; 
+
+let prenom = "Victor";
+let nom = "Hugo";
+let nomComplet = prenom+ nom; //"VictorHugo"
+
+let name = "George";
+    name = name + "Sand" // name += "Sand" 
